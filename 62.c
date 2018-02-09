589dd6298584639b5dc4204607b99f4e94bc2a25
saranyamano <31506060+saranyamano@users.noreply.github.com>
2018-02-09 10:24:27 +0000

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char a[10];
+int i;
+printf("enter the string\n");
+scanf("%s",&a);
+if(a[i]=='0'||a[i]=='1')
+{
+printf("string is represented in binary ");
+}
+else
+{
+printf("string is not represented in binary ");
+}
+return 0;
+}
