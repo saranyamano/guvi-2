b2964e8cf0e308adbef95fde3594a7903a513720
saranyamano <31506060+saranyamano@users.noreply.github.com>
2018-02-09 11:09:29 +0000

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(0
+{
+int n,m;
+printf("enter the values of n and m:\n");
+scanf("%d %d",&n,&m);
+if((n+m)%2==0)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
+}
+return 0;
+}
