376a3dc424bf0c76bd748b161daa19e3213fd661
saranyamano <31506060+saranyamano@users.noreply.github.com>
2018-02-26 16:33:30 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int area,br,len;
+printf("Enter the length:");
+scanf("%d",&len);
+printf("Enter the breadth:");
+scanf("%d",&br);
+area=len*br;
+printf("Area:%d",area);
+}
