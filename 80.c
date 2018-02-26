5557041f7ff9ddb60891ec70127f4d2309cb855b
saranyamano <31506060+saranyamano@users.noreply.github.com>
2018-02-26 16:00:34 +0530

    Create 80.c

---
+++ 80.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int n,sum=0,s,t;
+printf("enter the number:\n");
+scanf("%d",&n);
+while(n!=0)
+{
+s=n%10;
+sum=(sum*10)+s;
+n=n/10;
+}
+while(sum!=0)
+{
+t=sum%10;
+}
+if(t%2!=0)
+{
+printf("%d",t);
+}
+sum=sum/10;
+}
