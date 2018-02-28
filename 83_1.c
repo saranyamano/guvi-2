8681a565552b3dcee1d56d18eb6f2a55f3e5fc30
saranyamano <31506060+saranyamano@users.noreply.github.com>
2018-02-28 15:58:18 +0530

    Rename 83.c to 83_1.c

---
+++ 83_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int a1,a2,b1,b2;
+scanf("%d,%d",&a1,&a2);
+scanf("%d,%d",&b1,&b2);
+printf("\n");
+printf("%d\t%d\n",a1,a2);
+printf("%d\t%d\n",b1,b2);
+printf("%d\n",(a1/a2));
+printf("%d\n",(b1%b2));
+}
