86c00a805ab6634a56d7ca545ea0cfcac3d93bee
saranyamano <31506060+saranyamano@users.noreply.github.com>
2018-02-28 15:48:50 +0530

    Create 90.c

---
+++ 90.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char str[10];
+int len,i;
+scanf("%s",str);
+len=strlen(str);
+for(i=0;i<l;i++)
+{
+if(str[i]>='1'&&str[i]<='9')
+{
+printf("%d",str[i]);
+}
+}
+}
